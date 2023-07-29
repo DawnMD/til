0536f32533cc2d176596eb40e249799552dffb37
Mainak <mainakdas104@gmail.com>
2023-07-29 23:39:13 +0530

    Fix: Use til router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { tilRouter } from "./routers/tils";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  til: tilRouter,
 });
 
 // export type definition of API
