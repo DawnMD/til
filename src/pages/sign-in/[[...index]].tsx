2dd976688ccdbcd6e61890cf9941d0b5b7f24d35
Mainak <mainakdas104@gmail.com>
2023-07-30 14:02:35 +0530

    Fix: Improve signin page

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,17 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
-const SignInPage = () => (
-  <div style={styles}>
-    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-  </div>
-);
+const SignInPage = () => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <div className="absolute z-[1000] flex h-screen w-screen items-center justify-center backdrop-blur-sm">
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        signUpUrl="/sign-up"
+        appearance={{
+          baseTheme: resolvedTheme === "dark" ? dark : undefined,
+        }}
+      />
+    </div>
+  );
+};
 
 export default SignInPage;
-
-const styles = {
-  width: "100vw",
-  height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
