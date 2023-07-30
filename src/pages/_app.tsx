f641a1bd0d8ec8ca973fbc2c60417e80b8cb5da8
Mainak <mainakdas104@gmail.com>
2023-07-30 14:00:58 +0530

    Feat: Add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Layout } from "~/components/Layout";
+import { ThemeProvider } from "~/components/ThemeProvider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </ClerkProvider>
   );
 };
